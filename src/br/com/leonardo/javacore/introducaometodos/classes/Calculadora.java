2492af8bcbdee78f66c52d8ed65f769a4cba111f
Leonardo <leonardo.skt1107@gmail.com>
2023-03-10 22:49:50 -0300

    add calc

--- src/br/com/leonardo/javacore/introducaometodos/classes/Calculadora.java
+++ src/br/com/leonardo/javacore/introducaometodos/classes/Calculadora.java
@@ -4,4 +4,13 @@ public class Calculadora {
     public void somaDoisNumeros(){      //void = sera executado metodo que não retornará nada.
         System.out.println(5+5);
     }     
+
+    public void subtraiDoisNumeros(){
+        System.out.println(533-9);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2){      //num1 e num2 são parametros
+        System.out.println(num1*num2);;
+
+    }
 }
--- src/br/com/leonardo/javacore/introducaometodos/test/CalculadoraTest.java
+++ src/br/com/leonardo/javacore/introducaometodos/test/CalculadoraTest.java
@@ -5,5 +5,10 @@ import br.com.leonardo.javacore.introducaometodos.classes.Calculadora;
 public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
+        calc.somaDoisNumeros();     //somaDoisNumeros é o metodo = (), Atributos não usam ().
+        calc.subtraiDoisNumeros();
+        calc.multiplicaDoisNumeros(5, 5); //colocar os numeros nos parametros é = argumento
+        System.out.println("Continua execução!");
+        
     }
 }

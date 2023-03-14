ce4217921f940eb5147468c386fa9df82bc05b20
Leonardo <leonardo.skt1107@gmail.com>
2023-03-13 22:17:53 -0300

    add exercises calc

--- src/br/com/leonardo/javacore/introducaometodos/classes/Calculadora.java
+++ src/br/com/leonardo/javacore/introducaometodos/classes/Calculadora.java
@@ -1,6 +1,7 @@
 package br.com.leonardo.javacore.introducaometodos.classes;
 
 public class Calculadora {
+    
     public void somaDoisNumeros() { // void = sera executado metodo que não retornará nada.
         System.out.println(5 + 5);
     }
